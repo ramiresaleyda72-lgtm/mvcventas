191fd4230200f083ab9b9f981b9c1f1c74038e5e
aleyda <aleydaramirez@gmail.com>
2025-11-18 14:08:43 -0400

    mi tercer commit

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 from controllers import usuario_controller, cliente_controller, producto_controller, venta_controller
 from database import db
@@ -23,7 +23,7 @@ def inject_active_path():
 
 @app.route("/")
 def home():
-    return "<h1>Aplicacion ventas</h1>"
+    return render_template('base.html')
 
 if __name__ == "__main__":
     with app.app_context():
